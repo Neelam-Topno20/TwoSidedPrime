5ed17aa7d8aff51025f964abf8b9f4579f1f92a9
Neelam-Topno20 <neilgp6555@gmail.com>
2019-12-30 16:16:05 +0530

    updated code

---
+++ service/FindTSPService.py
@@ -0,0 +1,52 @@
+import math
+
+
+class FindTSPService():
+
+    @staticmethod
+    def find_prime(number):
+        if number == 2 or number == 3:
+            return True
+        sq = int(math.sqrt(number))
+        for i in range(2, sq + 1):
+            if number % i == 0:
+                return False
+        return True
+
+    @staticmethod
+    def find_tsp_service(number):
+        if number <= 1:
+            return False
+        flag = FindTSPService().find_prime(number)
+        if flag:
+            tmp = number
+            count = 0
+            tmp = int(tmp / 10)
+            while tmp != 0:
+                count = count + 1
+                print(tmp)
+                if tmp <= 1:
+                    return False
+                flag = FindTSPService().find_prime(number)
+                if not flag:
+                    return False
+                tmp = int(tmp / 10)
+            tmp = number
+            if int(tmp / 10) != 0:
+                tmp = tmp % (10 * count)
+            else:
+                tmp = 0
+            while tmp != 0:
+                count = count - 1
+                if tmp <= 1:
+                    return False
+                flag = FindTSPService().find_prime(number)
+                if not flag:
+                    return False
+                if int(tmp / 10) == 0:
+                    tmp = 0
+                else:
+                    tmp = tmp % (10 * count)
+        else:
+            return False
+        return True
